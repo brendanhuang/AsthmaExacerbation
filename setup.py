c51a2bdb3a09f2a4d3bca147b6c94d0371a90f51
brendan-huang <brendan.huang@ucsf.edu>
2023-06-28 16:15:47 -0700

    cookiecutter layout

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Query DEID-CDW for Asthma Exacerbations',
+    author='Brendan Huang',
+    license='MIT',
+)
